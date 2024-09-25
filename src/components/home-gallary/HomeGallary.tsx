e2cd6d232f5e8be47fa327a9311ee0091fd5b38b
WIsavachart <wisavachart.s@gmail.com>
2024-09-25 18:07:09 +0700

    Home Finish

--- src/components/home-gallary/HomeGallary.tsx
+++ src/components/home-gallary/HomeGallary.tsx
@@ -1,11 +1,9 @@
+import { HomeGallaryImage } from "../../utils/images";
+
 const HomeGallary = () => {
   return (
     <div className="relative mx-4 flex aspect-square overflow-hidden max_mb:h-[450px] desktop:flex-1 desktop:mr-0 desktop:h-full 2xl:max-w-[700px]">
-      <img
-        className="object-cover w-full "
-        src="/src/assets/images/text_img.jpg"
-        alt=""
-      />
+      <img className="object-cover w-full " src={HomeGallaryImage[0]} alt="" />
     </div>
   );
 };
---
+++ src/utils/images.tsx
@@ -0,0 +1,2 @@
+import pic1 from "/src/assets/images/text_img.jpg";
+export const HomeGallaryImage = [pic1];
